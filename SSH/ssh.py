42970a8f1959db5312e5023df3e64b9e1e441ab1
lue liu <lue_liu@qq.com>
2013-06-24 21:59:58 +0800

    paramiko sudo

--- SSH/ssh.py
+++ SSH/ssh.py
@@ -5,15 +5,16 @@
 
 import paramiko
     
-hostname='192.168.122.10'
-username='lue'
-password='1iu1u3724'
+host='192.168.122.10'
+user='lue'
+pwd='123456'
+cmd='sudo ifconfig;df -h'
          
 if __name__=='__main__':    
-        paramiko.util.log_to_file('paramiko.log')    
-        s=paramiko.SSHClient()     
-        s.set_missing_host_key_policy(paramiko.AutoAddPolicy())    
-        s.connect(hostname = hostname,username=username, password=password)    
-        stdin,stdout,stderr=s.exec_command('ifconfig;free;df -h')    
-        print stdout.read()    
-        s.close()
+    paramiko.util.log_to_file('paramiko.log')    
+    s=paramiko.SSHClient()     
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())    
+    s.connect(host,username=user, password=pwd)    
+    stdin,stdout,stderr=s.exec_command(cmd)    
+    print stdout.read()    
+    s.close()
