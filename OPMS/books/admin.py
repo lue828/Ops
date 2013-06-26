cfcc512972a8cc124b125727938f9fc7278b5d94
lue liu <lue_liu@qq.com>
2013-06-26 22:27:30 +0800

    Change admin.py

--- OPMS/books/admin.py
+++ OPMS/books/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import import admin
-from Ops.books.models import Publisher, Author, Book
+from django.contrib import admin
+from books.models import Publisher, Author, Book
 
 admin.site.register(Publisher)
 admin.site.register(Author)
