ef52d3dd2b92b410f89e04eb5baed0bb994d1275
lue liu <lue_liu@qq.com>
2013-06-27 13:02:21 +0800

    Change html

--- OPMS/books/views.py
+++ OPMS/books/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
-
-def search_form(request):
-    return render_to_response('search_form.html')
+from books.models import Book
 
 def search(request):
+    error = False
     if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+        q = request.GET['q']
+        if not q:
+            error =True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html', {'books': books, 'query': q})
+    return render_to_response('search_form.html', {'error': error})
