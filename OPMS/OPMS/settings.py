ce39a7e2b6ec89b16d7ff868135e93d58c57660d
lue liu <lue_liu@qq.com>
2013-06-27 13:25:26 +0800

    Change template

--- OPMS/OPMS/settings.py
+++ OPMS/OPMS/settings.py
@@ -13,7 +13,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#db for home
 #        'NAME': 'C:\\Users\\administrator\\git\\Ops\\OPMS\\database\\sqlite.db',                      # Or path to database file if using sqlite3.
+#db for office
         'NAME': 'C:\\Users\\Madhouse\\git\\Ops\\OPMS\\database\\sqlite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
