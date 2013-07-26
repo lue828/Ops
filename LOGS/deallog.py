494dd7df266cb7496c2b0d236a1598481df3c5d5
lue liu <lue_liu@qq.com>
2013-07-26 11:36:03 +0800

    Change deallog.py

--- LOGS/deallog.py
+++ LOGS/deallog.py
@@ -10,123 +10,95 @@ f = open('SmartMAD2_error_130724.log')
 
 for line in f:
     rpe= re.compile(r'pe=.*').findall(line)
-    for e in rpe:
-        cpe = e.split('&')[0].split('pe=')[1]
-        if cpe:
-            pe = cpe
-        else:
-            pe = None
+    if rpe:
+        pe = rpe.split('&')[0].split('pe=')[1]
+    else:
+        pe = None
 
     rmod = re.compile(r'mod=.*').findall(line)
-    for m in rmod:
-        cmod = m.split('&')[0].split('mod=')[1]
-        if cmod:
-            mod = cmod
-        else:
-            mod = None
+    if rmod:
+        mod = rmom.split('&')[0].split('mod=')[1]
+    else:
+        mod = None
         
         
     ros = re.compile(r'os=.*').findall(line)
-    for o in ros:
-        cos = o.split('&')[0].split('os=')[1]
-        if cos:
-            dos = cos
-        else:
-            dos = None 
+    if ros:
+        dos = ros.split('&')[0].split('os=')[1]
+    else:
+        dos = None 
         
     rosv = re.compile(r'osv=.*').findall(line)
-    for sv in rosv:
-        cosv = sv.split('&')[0].split('osv=')[1]
-        if cosv:
-            osv = cosv
-        else:
-            osv = None
+    if rosv:
+        osv = rosv.split('&')[0].split('osv=')[1]
+    else:
+        osv = None
         
     rcpu = re.compile(r'cpu=.*').findall(line)
-    for c in rcpu:
-        ccpu = c.split('&')[0].split('cpu=')[1]
-        if ccpu:
-            cpu = ccpu
-        else:
-            cpu = None
+    if rcpu:
+        cpu = rcpu.split('&')[0].split('cpu=')[1]
+    else:
+        cpu = None
         
     rram = re.compile(r'ram=.*').findall(line)
-    for ra in rram:
-        cram = ra.split('&')[0].split('ram=')[1]
-        if cram:
-            ram = cram
-        else:
-            ram = None
+    if rram:
+        ram = rram.split('&')[0].split('ram=')[1]
+    else:
+        ram = None
 
     rrom = re.compile(r'rom=.*').findall(line)
-    for ro in rrom:
-        crom = ro.split('&')[0].split('rom=')[1]
-        if crom:
-            rom = crom
-        else:
-            rom = None
+    if rrom:
+        rom = rrom.split('&')[0].split('rom=')[1]
+    else:
+        rom = None
         
     rjb = re.compile(r'jb=.*').findall(line)
-    for j in rjb:
-        cjb = j.split('&')[0].split('jb=')[1]
-        if cjb:
-            jb = cjb
-        else:
-            jb = None
+    if rjb:
+        jb = rjb.split('&')[0].split('jb=')[1]
+    else:
+        jb = None
    
     rde = re.compile(r'de=.*').findall(line)
-    for d in rde:
-        cde = d.split('&')[0].split('de=')[1]
-        if cde:
-            de = cde
-        else:
-            de = None
+    if rde:
+        de = rde.split('&')[0].split('de=')[1]
+    else:
+        de = None
     
     rapn = re.compile(r'apn=.*').findall(line)
-    for ap in rapn:
-        capn = ap.split('&')[0].split('apn=')[1]
-        if capn:
-            apn = cpan
-        else:
-            apn = None
+    if rapn:
+        apn = rapn.split('&')[0].split('apn=')[1]
+    else:
+        apn = None
 
     rav = re.compile(r'av=.*').findall(line)
-    for rv in rav:
-        cav = rv.split('&')[0].split('av=')[1]
-        if cav:
-            av = cav
-        else:
-            av = None
+    if rav:
+        av = rav.split('&')[0].split('av=')[1]
+    else:
+        av = None
 
     rmem = re.compile(r'mem=.*').findall(line)
-    for me in rmem:
-        cmem = me.split('&')[0].split('mem=')[1]
-        if cmem:
-            mem = cmem
-        else:
-            mem = None
+    if rmem:
+        mem = rmem.split('&')[0].split('mem=')[1]
+    else:
+        mem = None
         
     rpv = re.compile(r'pv=.*').findall(line)
-    for v in rpv:
-        cpv = v.split('&')[0].split('pv=')[1]
-        if cpv:
-            pv = cpv
-        else:
-            pv = None
+    if rpv:
+        pv = rpv.split('&')[0].split('pv=')[1]
+    else:
+        pv = None
 
     rua = re.compile(r'ua=.*').findall(line)
-    for a in rua:
-        cua = a.split('&')[0].split('ua=')[1]
-        if cua:
-            ua = cua
-        else:
-            ua = None
+    if rua:
+        ua = rua.split('&')[0].split('ua=')[1]
+    else:
+        ua = None
 
 
     #Insert into database. 
-    conn = MySQLdb.connect(host='172.16.27.6', user='ops', passwd='madhouse', db='madhouse', charset='utf8')
+    conn = MySQLdb.connect(host='192.168.200.200', user='ops', passwd='madhouse', db='madhouse', charset='utf8')
     cur = conn.cursor()
-    cur.execute("insert into deallog_deallog(dt, pe, mod, dos, osv, cpu, ram, rom, jb, de, apn, av, mem, pv, ua) value(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (dt, pe, mod, os, osv, cpu, ram, rom, jb, de, apn, av, mem, pv, ua))
+    cur.execute("insert into deallog_deallog(dt, pe, mod, dos, osv, cpu, ram, rom, jb, de, apn, av, mem, pv, ua) value(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (dt, pe, mod, dos, osv, cpu, ram, rom, jb, de, apn, av, mem, pv, ua))
     conn.commit()
     cur.close()
     conn.close()
