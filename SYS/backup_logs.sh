a1b3199b16ee7e627f3e26afc13d408eb362b18f
lue liu <lue_liu@qq.com>
2013-09-09 17:50:09 +0800

    Change backup_logs.sh

--- SYS/backup_logs.sh
+++ SYS/backup_logs.sh
@@ -8,14 +8,17 @@ for i in 91 92 93 94 95 96 97 98 237 238 239 240 241
 do
     echo "IP:"${src}.${i}
     #To detect the directory exists
-    ssh -p 40109 madhouse@${dest} "if [ ! -d /${storage}/backup/${src}.${i}${dir} ];then 'Directory is not exist, and by created.';mkdir -p /${storage}/backup/${src}.${i}${dir};else echo 'Directory is exist, and syncing';fi"
+    ssh -p 40109 -o StrictHostKeyChecking=no madhouse@${dest} "if [ ! -d /${storage}/backup/${src}.${i}${dir} ];then 'Directory is not exist, and by created.';mkdir -p /${storage}/backup/${src}.${i}${dir};else echo 'Directory is exist, and syncing';fi"
 
     #Rsync data to a backup server
-    ssh madhouse@${src}.${i} "/usr/local/bin/rsync -avzP --exclude "*.log" -e 'ssh -o UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no' -e 'ssh -p 40109 ' ${dir}/  madhouse@${dest}:/${storage}/backup/${src}.${i}${dir}/"
+    ssh -o StrictHostKeychecking=no madhouse@${src}.${i} "/usr/local/bin/rsync -avzP --exclude "*.log" -e 'ssh -o UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no' -e 'ssh -p 40109 ' ${dir}/  madhouse@${dest}:/${storage}/backup/${src}.${i}${dir}/"
 
     #Delete files before 10 days
     if [ $? -eq 0 ]
     then
-        ssh madhouse@${src}.${i} "sudo find $dir/ -ctime +10 -type f -exec rm {} \;"
+        ssh -o StrictHostKeyChecking=no madhouse@${src}.${i} "sudo find $dir/ -ctime +10 -type f -exec rm {} \;"
     fi
 done
+
+
+#rsync -avzP --remove-source-files 可以删除源文件 
