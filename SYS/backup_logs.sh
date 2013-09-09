119cd12965e9711c8795c83ac45c4c6bc2351b8e
lue liu <lue_liu@qq.com>
2013-09-09 16:07:47 +0800

    Add backup_logs.sh

---
+++ SYS/backup_logs.sh
@@ -0,0 +1,21 @@
+#!/usr/local/bin/bash
+src=10.10.8
+dest=220.189.221.51
+dir=/services/http_logs
+storage=datan01
+
+for i in 91 92 93 94 95 96 97 98 237 238 239 240 241
+do
+    echo "IP:"${src}.${i}
+    #To detect the directory exists
+    ssh -p 40109 madhouse@${dest} "if [ ! -d /${storage}/backup/${src}.${i}${dir} ];then 'Directory is not exist, and by created.';mkdir -p /${storage}/backup/${src}.${i}${dir};else echo 'Directory is exist, and syncing';fi"
+
+    #Rsync data to a backup server
+    ssh madhouse@${src}.${i} "/usr/local/bin/rsync -avzP --exclude "*.log" -e 'ssh -o UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no' -e 'ssh -p 40109 ' ${dir}/  madhouse@${dest}:/${storage}/backup/${src}.${i}${dir}/"
+
+    #Delete files before 10 days
+    if [ $? -eq 0 ]
+    then
+        ssh madhouse@${src}.${i} "sudo find $dir/ -ctime +10 -type f -exec rm {} \;"
+    fi
+done
